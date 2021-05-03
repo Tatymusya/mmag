9738eb5a15ab4e2313aa26b4e8d7cc13cbd6031d
Tatya <tatykolcova@gmail.com>
2021-05-04 01:22:46 +0300

    Add structure

---
+++ server/app.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/extensions */
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import cors from 'cors';
+import morgan from 'morgan';
+import config from '../webpack/webpack.dev.config.js';
+import createServerAndListen from './lib/server.js';
+
+const corsOptions = {
+  origin: 'https://localhost:9007'
+};
+const dirname = path.resolve();
+const compiler = webpack(config);
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(webpackDevMiddleware(compiler, config.devServer));
+app.use(webpackHotMiddleware(compiler));
+app.use('/fonts', express.static('../dist/'));
+
+const main = async () => {
+  try {
+    createServerAndListen(app)
+      .then(() => {
+        job.start();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(dirname, 'dist', 'index.html'));
+});
+
+main();
